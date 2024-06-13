e5e3077c0012c1d5ebf8edf519b82047eaeccafc
yuyunaginagi <centrepg8@gmail.com>
2024-06-13 20:45:10 +0900

    練習問題

--- question2.ts
+++ question2.ts
@@ -1,6 +1,6 @@
-const ProductType = {
-  beverage: 0,
-  food:     1,
-  books:    2,
+enum ProductType = {
+  beverage,
+  food,
+  books,
 }
-console.log(ProductType.beverage)
+console.log(Product.beverage)
